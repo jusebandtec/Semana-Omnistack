d568ac35493e64327d6dd62ae494b868b3476e29
jusebandtec <jose.silva@bandtec.com.br>
2020-03-25 23:52:28 -0300

    Desenvolvendo o front-end da app

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+//JSX (javascript xml)
+
+ 
+ function App() {
+/**const [counter, setCounter] = useState(0);
+
+// Array [valor, funçãoDeAtualização]
+
+  function increment () {
+    setCounter(counter + 1);
+ }n
+*/
+  return (
+    <Routes />
+  );
+}
+
+export default App;
